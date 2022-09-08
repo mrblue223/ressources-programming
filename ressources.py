dea30943d1ebcb989e93cac881508e8d4c4392c3
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2022-09-08 09:04:38 -0700

    Update ressources.py

--- ressources.py
+++ ressources.py
@@ -1,1 +1,8 @@
-
+# PYTHON
+https://www.youtube.com/watch?v=rfscVS0vtbw (python in 4 hours)
+https://www.youtube.com/playlist?list=PL0-84-yl1fUnRuXGFe_F7qSH1LEnn9LkW (how to automate the boring stuff playlist)
+https://www.thepythoncode.com/topic/ethical-hacking (projects for ethical hackers)
+  
+  
+#RESSOURCES DUMP
+https://www.w3schools.com/default.asp (HTML, JS, CSS, and more)
