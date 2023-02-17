2be3cb22d5675139dba04704be4acfe9a2439324
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2023-02-17 13:14:56 -0800

    Update ressources.py

--- ressources.py
+++ ressources.py
@@ -2,6 +2,7 @@
 https://www.youtube.com/watch?v=rfscVS0vtbw (python in 4 hours)
 https://www.youtube.com/playlist?list=PL0-84-yl1fUnRuXGFe_F7qSH1LEnn9LkW (how to automate the boring stuff playlist)
 https://www.thepythoncode.com/topic/ethical-hacking (projects for ethical hackers)
+https://www.binarytides.com/category/programming/sockets/ (www.binarytides.com)
   
   
 #RESSOURCES DUMP
