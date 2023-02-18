7be4422fe8098dcf95fcb8c1e28b13640ac8f158
mrblue223 <96673202+mrblue223@users.noreply.github.com>
2023-02-18 16:46:58 -0500

    Update ressources.py

--- ressources.py
+++ ressources.py
@@ -3,8 +3,31 @@ https://www.youtube.com/watch?v=rfscVS0vtbw (python in 4 hours)
 https://www.youtube.com/playlist?list=PL0-84-yl1fUnRuXGFe_F7qSH1LEnn9LkW (how to automate the boring stuff playlist)
 https://www.thepythoncode.com/topic/ethical-hacking (projects for ethical hackers)
 https://www.binarytides.com/category/programming/sockets/ (www.binarytides.com)
+
+# BASH
+https://www.youtube.com/playlist?list=PLT98CRl2KxKGj-VKtApD8-zCqSaN2mD4w (Bash scripting playlist)
+https://www.freecodecamp.org/news/shell-scripting-crash-course-how-to-write-bash-scripts-in-linux/ (bash scripting website)
+https://linuxconfig.org/bash-scripting-cheat-sheet (Cheatsheet)
+https://devhints.io/bash (cheeatsheet 1)
+https://b-ok.cc/book/782878/871e69 (Learning the bash shell)
+https://b-ok.cc/book/3382904/7f1b79?dsource=recommend (bash cookbook)
+https://linuxhandbook.com/if-else-bash/ (if statement)
+https://www.geeksforgeeks.org/bash-scripting-while-loop/ (while loop)
+https://www.howtogeek.com/766978/how-to-use-case-statements-in-bash-scripts/ (case statement)
+https://www.shellscript.sh/tips/banner/ (banner)
   
+# HTML-CSS
+https://www.sarasoueidan.com/blog/css-shapes/ (Creating Non-Rectangular Layouts)
+https://labs.jensimmons.com/ (jensimons)
+https://www.onblastblog.com/css3-cheat-sheet/ (CSS cheatsheet)
+https://css-tricks.com/ (css-tricks)
+https://csswizardry.com/ (css wizadry)
+https://codepen.io/2016/popular/pens/ (codepen)
+https://web-design-weekly.com/ (web design weekly)
+https://digital.com/tools/html-cheatsheet/ (html cheatsheet)
+
   
 #RESSOURCES DUMP
 https://www.w3schools.com/default.asp (HTML, JS, CSS, and more)
 https://www.instagram.com/direct/t/340282366841710300949128244362696895053 (codeacademy)
+https://www.youtube.com/watch?v=PPQ8m8xQAs8&t=760s (Nullbyte bash)
